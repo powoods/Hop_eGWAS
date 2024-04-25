248204b81ec6114f3e123682a87c80618df05b3f
Patrick Woods <patrickodwoods@gmail.com>
2024-04-25 10:38:07 -0600

    Updating the scripts to extract lower resolution climate data and conduct GWAS using 1 sample per population.

---
+++ GWAS_using_1_sample_per_population.R
@@ -0,0 +1,90 @@
+### Script for categorizing lon/lat data into factors and sampling 1 plant from each factor ###
+#Author Patrick Woods #
+# Date: April 19, 2024 #
+
+
+# Read in the raw metadata file supplied by UMN and subset for only genotyeps that are considered 'Wild' #
+cords <- read.csv('meta_for_patrick.csv',header=T)
+wild <- subset(cords, cords$Status == 'Wild')
+
+# Make a new column that combines every longitude + latitude coordinate into a single column #
+wild$cat <- paste(wild$lon, wild$lat, sep="_")
+
+wild$cat <- as.factor(wild$cat)
+
+str(wild) # 249 unique lon/lat coordinate combinations.
+
+# Randomly select 1 sample from each unique longitude_latitude combination #
+
+library(dplyr)
+
+wild_sub <- wild %>%
+  group_by(cat) %>%
+  slice_sample(n = 1)
+
+# Subset the extracted bioclimate variable data for the subset of 249 genotypes #
+
+phe <- read.csv('final_climate_data_resolution_10_trim.csv',header=T)
+
+wild_sub_phe <- left_join(wild_sub, phe, by = 'Genotype')
+wild_sub_phe <- as.data.frame(wild_sub_phe)
+write.csv(wild_sub_phe, file = 'wild_sub_phe.csv', row.names = F, quote = F)
+
+# Test each of the 19 bioclimate variables for normality amongst the 249 gneotype subset #
+p_vals <- data.frame()
+
+traits <- 10:ncol(wild_sub_phe)
+
+for (i in traits){
+  shap <- shapiro.test(wild_sub_phe[,i])
+  p_vals <- rbind(p_vals, shap$p.value)
+}
+
+max(p_vals) # greatest p value is still less than 0.05. Will quantile normalize the traits.
+
+# all traits show significant departure from normality, will use quantile normalization to transform #
+
+trans <- data.frame()
+
+library(bestNormalize)
+library(rowr)
+
+for (i in traits) {
+  trait_qn <- orderNorm(wild_sub_phe[,i])
+  df <- as.data.frame(trait_qn$x.t)
+  colnames(df) <- colnames(wild_sub_phe[i])
+  trans<- cbind.fill(trans,df,fill = NA)
+}
+
+p_vals_trans <- data.frame()
+
+traits <- 2:ncol(trans)
+
+for (i in traits){
+  shap <- shapiro.test(trans[,i])
+  p_vals_trans <- rbind(p_vals_trans, shap$p.value)
+}
+
+max(p_vals_trans)
+
+write.csv(trans, file = 'bioclim_qn_unique_locations.csv', row.names = F, quote=F)
+
+# Convert the .csv file to a .txt file using excel #
+
+### load GAPIT functions ###
+source("http://zzlab.net/GAPIT/GAPIT.library.R")
+source("http://zzlab.net/GAPIT/gapit_functions.txt")
+
+myGD=read.delim(file="wild_sub_maf05.hmp.txt",header=FALSE,sep="\t")
+myY <- read.table('bioclim_qn_unique_locations_final_GAPIT.txt',header=T)
+#install.packages("bigmemory",repos = "http://cran.us.r-project.org")
+#install.packages("biganalytics",repos = "http://cran.us.r-project.org")
+#install.packages("devtools",repos = "http://cran.us.r-project.org")
+#install.packages("scatterplot3d",repos = "http://cran.us.r-project.org")
+
+### GWAS using BLINK model and 5 PCs ###
+myGAPIT <- GAPIT(
+  Y=myY[c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20)],
+  G=myGD,
+  PCA.total=5,
+  model=c("Blink"), SNP.MAF = 0.05)
--- world_clim_data_script.R
+++ world_clim_data_script.R
@@ -1,21 +1,25 @@
-###Script for downloading Climate Data from World Clim and Using it to do GWAS###
+###Script for downloading Climate Data from World Clim for Use in GWAS###
 ##Author: Patrick Woods##
-##Date: April 9, 2024##
+##Date: April 25, 2024##
 
 setwd("Desktop/Projects/eGWAS/")
 install.packages("raster")
 
-install.packages(file.choose(), repos=NULL, type='source') #Needed to download and manually install terra_1.7-71.tar
+#install.packages(file.choose(), repos=NULL, type='source') #Needed to download and manually install terra_1.7-71.tar
 library(raster)
 
 #need a file with coordinates
-coords <- read.csv(file.choose(),header=T) #Need a .csv file with longitude and latitude coordinates for individual samples
+coords <- read.csv("meta_for_patrick.csv",header=T) #Need a .csv file with longitude and latitude coordinates for individual samples
+
+#Subset file for 'Wild' status accessions only
+coords_wild <- subset(coords, coords$Status == 'Wild')
 
 #import all climate data
-r <- getData('worldclim',var='bio', res=2.5) #Can adjust the resolution
+r <- getData('worldclim',var='bio', res=10) #resolution set to 10 arcminutes to account for uncertainty in geographic locations
 
-clim1 <- coords$longitude
-clim2 <- coords$latitude
+#Extract climate data for coordinates of wild accessions
+clim1 <- coords_wild$lon
+clim2 <- coords_wild$lat
 clim1x <- c(clim1)
 clim2y <- c(clim2)
 df <- data.frame(x = clim1x, y = clim2y)
@@ -23,4 +27,6 @@ points <- SpatialPoints(df, proj4string = r@crs)
 extrct <- extract(r, points)
 cods <- cbind.data.frame(coordinates(points), extrct)
 
+cods <- cbind(coords_wild$Genotype, cods)
 
+write.csv(final_clim_data, file = "final_climate_data_resolution_10_trim.csv")
